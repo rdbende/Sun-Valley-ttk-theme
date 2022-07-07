891820a07ad5ed02efe771b4e4c13c6a17ab30c3
rdbende <rdbende@gmail.com>
2022-07-07 11:49:22 +0200

    Fixes #35

--- sun-valley.tcl
+++ sun-valley.tcl
@@ -1,4 +1,4 @@
-# Copyright © 2021 rdbende <rdbende@gmail.com>
+# Copyright © 2021-2022 rdbende <rdbende@gmail.com>
 
 source [file join [file dirname [info script]] theme light.tcl]
 source [file join [file dirname [info script]] theme dark.tcl]
@@ -6,16 +6,16 @@ source [file join [file dirname [info script]] theme dark.tcl]
 option add *tearOff 0
 
 proc set_theme {mode} {
-	if {$mode == "dark"} {
-		ttk::style theme use "sun-valley-dark"
+    if {$mode == "dark"} {
+        ttk::style theme use "sun-valley-dark"
 
-		array set colors {
-		    -fg             "#ffffff"
-		    -bg             "#1c1c1c"
-		    -disabledfg     "#595959"
-		    -selectfg       "#ffffff"
-		    -selectbg       "#2f60d8"
-		}
+        array set colors {
+            -fg             "#ffffff"
+            -bg             "#1c1c1c"
+            -disabledfg     "#595959"
+            -selectfg       "#ffffff"
+            -selectbg       "#2f60d8"
+        }
         
         ttk::style configure . \
             -background $colors(-bg) \
@@ -26,13 +26,13 @@ proc set_theme {mode} {
             -selectforeground $colors(-selectfg) \
             -insertwidth 1 \
             -insertcolor $colors(-fg) \
-            -fieldbackground $colors(-selectbg) \
-            -font {"Segoe Ui" 10} \
+            -fieldbackground $colors(-bg) \
+            -font {"Segoe UI" 10} \
             -borderwidth 0 \
             -relief flat
 
         tk_setPalette \
-        	background [ttk::style lookup . -background] \
+            background [ttk::style lookup . -background] \
             foreground [ttk::style lookup . -foreground] \
             highlightColor [ttk::style lookup . -focuscolor] \
             selectBackground [ttk::style lookup . -selectbackground] \
@@ -43,19 +43,17 @@ proc set_theme {mode} {
         ttk::style map . -foreground [list disabled $colors(-disabledfg)]
 
         option add *font [ttk::style lookup . -font]
-        option add *Menu.selectcolor $colors(-fg)
-        option add *Menu.background #2f2f2f
     
-	} elseif {$mode == "light"} {
-		ttk::style theme use "sun-valley-light"
+    } elseif {$mode == "light"} {
+        ttk::style theme use "sun-valley-light"
 
-		array set colors {
-		    -fg             "#202020"
-		    -bg             "#fafafa"
-		    -disabledfg     "#a0a0a0"
-		    -selectfg       "#ffffff"
-		    -selectbg       "#2f60d8"
-		}
+        array set colors {
+            -fg             "#202020"
+            -bg             "#fafafa"
+            -disabledfg     "#a0a0a0"
+            -selectfg       "#ffffff"
+            -selectbg       "#2f60d8"
+        }
 
         ttk::style configure . \
             -background $colors(-bg) \
@@ -66,23 +64,21 @@ proc set_theme {mode} {
             -selectforeground $colors(-selectfg) \
             -insertwidth 1 \
             -insertcolor $colors(-fg) \
-            -fieldbackground $colors(-selectbg) \
-            -font {"Segoe Ui" 10} \
+            -fieldbackground $colors(-bg) \
+            -font {"Segoe UI" 10} \
             -borderwidth 0 \
             -relief flat
 
-        tk_setPalette background [ttk::style lookup . -background] \
-            foreground [ttk::style lookup . -foreground] \
-            highlightColor [ttk::style lookup . -focuscolor] \
-            selectBackground [ttk::style lookup . -selectbackground] \
-            selectForeground [ttk::style lookup . -selectforeground] \
-            activeBackground [ttk::style lookup . -selectbackground] \
-            activeForeground [ttk::style lookup . -selectforeground]
+        tk_setPalette background $colors(-bg) \
+            foreground $colors(-fg) \
+            highlightColor $colors(-selectbg) \
+            selectBackground $colors(-selectbg) \
+            selectForeground $colors(-selectfg) \
+            activeBackground $colors(-selectbg) \
+            activeForeground $colors(-selectfg)
         
         ttk::style map . -foreground [list disabled $colors(-disabledfg)]
 
         option add *font [ttk::style lookup . -font]
-        option add *Menu.selectcolor $colors(-fg)
-        option add *Menu.background #e7e7e7
-	}
+    }
 }
