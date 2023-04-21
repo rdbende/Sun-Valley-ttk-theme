54da8adcafc45f8b5e117b3bfbc462aa11e9baf0
rdbende <rdbende@proton.me>
2023-04-21 23:36:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with (Path(__file__).parent / "README.md").open() as file:
 
 setup(
     name="sv_ttk",
-    version="2.4.3",
+    version="2.4.4",
     license="MIT",
     author="rdbende",
     author_email="rdbende@gmail.com",
