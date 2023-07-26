ea91020787a5961a40180909f7be6150fb595b1d
rdbende <rdbende@proton.me>
2023-07-26 21:22:28 +0200

    Get rid of hacky `set_theme` proc (#113)

--- sv_ttk/sv.tcl
+++ sv_ttk/sv.tcl
@@ -1,5 +1,9 @@
 package require Tk 8.6
 
+source [file join [file dirname [info script]] theme light.tcl]
+source [file join [file dirname [info script]] theme dark.tcl]
+
+
 if {[tk windowingsystem] == "win32"} {
   set static ""
 } else {
@@ -15,7 +19,8 @@ font create SunValleyTitleFont -family "Segoe UI Variable$static Display Semibol
 font create SunValleyTitleLargeFont -family "Segoe UI Variable$static Display Semibold" -size -40
 font create SunValleyDisplayFont -family "Segoe UI Variable$static Display Semibold" -size -68
 
-proc config_input_font {w} {
+
+proc config_entry_font {w} {
   set font_config [$w config -font]
   if {[lindex $font_config 3] != [lindex $font_config 4]} {
     return
@@ -25,52 +30,46 @@ proc config_input_font {w} {
   }
 }
 
-proc config_menus {w} {
-  if {[tk windowingsystem] != "aqua"} {
-    set theme [ttk::style theme use]
-    if {$theme == "sun-valley-dark"} {
-      $w configure \
-        -relief solid \
-        -borderwidth 1 \
-        -activeborderwidth 0 \
-        -background "#292929" \
-        -activebackground $ttk::theme::sv_dark::colors(-selbg) \
-        -activeforeground $ttk::theme::sv_dark::colors(-selfg) \
-        -selectcolor $ttk::theme::sv_dark::colors(-selfg)
-    } elseif {$theme == "sun-valley-light"} {
-      $w configure \
-        -relief solid \
-        -borderwidth 1 \
-        -activeborderwidth 0 \
-        -background "#e7e7e7" \
-        -activebackground $ttk::theme::sv_dark::colors(-selbg) \
-        -activeforeground $ttk::theme::sv_dark::colors(-selfg) \
-        -selectcolor $ttk::theme::sv_dark::colors(-selfg)
-    }
 
-    if {[[winfo toplevel $w] cget -menu] != $w} {
-      if {$theme == "sun-valley-dark"} {
-        $w configure -borderwidth 0 -background $ttk::theme::sv_dark::colors(-bg)
-      } elseif {$theme == "sun-valley-light"} {
-        $w configure -borderwidth 0 -background $ttk::theme::sv_light::colors(-bg)
-      }
+proc config_menus {w} {
+  if {[tk windowingsystem] == "aqua"} {
+    return
+  }
+
+  set theme [ttk::style theme use]
+  if {$theme == "sun-valley-dark"} {
+    $w configure \
+      -relief solid \
+      -borderwidth 1 \
+      -activeborderwidth 0 \
+      -background "#292929" \
+      -activebackground $ttk::theme::sv_dark::colors(-selbg) \
+      -activeforeground $ttk::theme::sv_dark::colors(-selfg) \
+      -selectcolor $ttk::theme::sv_dark::colors(-selfg)
+  } elseif {$theme == "sun-valley-light"} {
+    $w configure \
+      -relief solid \
+      -borderwidth 1 \
+      -activeborderwidth 0 \
+      -background "#e7e7e7" \
+      -activebackground $ttk::theme::sv_dark::colors(-selbg) \
+      -activeforeground $ttk::theme::sv_dark::colors(-selfg) \
+      -selectcolor $ttk::theme::sv_dark::colors(-selfg)
+  }
+
+  if {[[winfo toplevel $w] cget -menu] != $w} {
+    if {$theme == "sun-valley-dark"} {
+      $w configure -borderwidth 0 -background $ttk::theme::sv_dark::colors(-bg)
+    } elseif {$theme == "sun-valley-light"} {
+      $w configure -borderwidth 0 -background $ttk::theme::sv_light::colors(-bg)
     }
   }
 }
 
-bind TEntry <<ThemeChanged>> {config_input_font %W}
-bind TCombobox <<ThemeChanged>> {config_input_font %W}
-bind TSpinbox <<ThemeChanged>> {config_input_font %W}
-bind Menu <<ThemeChanged>> {config_menus %W}
 
-source [file join [file dirname [info script]] theme light.tcl]
-source [file join [file dirname [info script]] theme dark.tcl]
-
-
-proc set_theme {mode} {
-  if {$mode == "dark"} {
-    ttk::style theme use "sun-valley-dark"
-    
+proc configure_colors {} {
+  set theme [ttk::style theme use]
+  if {$theme == "sun-valley-dark"} {
     ttk::style configure . \
       -background $ttk::theme::sv_dark::colors(-bg) \
       -foreground $ttk::theme::sv_dark::colors(-fg) \
@@ -93,14 +92,9 @@ proc set_theme {mode} {
       selectForeground $ttk::theme::sv_dark::colors(-selfg) \
       activeBackground $ttk::theme::sv_dark::colors(-selbg) \
       activeForeground $ttk::theme::sv_dark::colors(-selfg)
-    
-    ttk::style map . -foreground [list disabled $ttk::theme::sv_dark::colors(-disfg)]
 
-    option add *tearOff 0
-  
-  } elseif {$mode == "light"} {
-    ttk::style theme use "sun-valley-light"
-    
+    ttk::style map . -foreground [list disabled $ttk::theme::sv_dark::colors(-disfg)]
+  } elseif {$theme == "sun-valley-light"} {
     ttk::style configure . \
       -background $ttk::theme::sv_light::colors(-bg) \
       -foreground $ttk::theme::sv_light::colors(-fg) \
@@ -123,9 +117,14 @@ proc set_theme {mode} {
       selectForeground $ttk::theme::sv_light::colors(-selfg) \
       activeBackground $ttk::theme::sv_light::colors(-selbg) \
       activeForeground $ttk::theme::sv_light::colors(-selfg)
-    
-    ttk::style map . -foreground [list disabled $ttk::theme::sv_light::colors(-disfg)]
 
-    option add *tearOff 0
+    ttk::style map . -foreground [list disabled $ttk::theme::sv_light::colors(-disfg)]
   }
 }
+
+
+bind [winfo class .] <<ThemeChanged>> {+configure_colors}
+bind TEntry <<ThemeChanged>> {+config_entry_font %W}
+bind TCombobox <<ThemeChanged>> {+config_entry_font %W}
+bind TSpinbox <<ThemeChanged>> {+config_entry_font %W}
+bind Menu <<ThemeChanged>> {+config_menus %W}
--- tests/test_sv_ttk.py
+++ tests/test_sv_ttk.py
@@ -5,41 +5,48 @@ import pytest
 import sv_ttk
 
 
-def test_with_explicit_root(root):
-    sv_ttk.set_theme("dark", root=root)
-    assert sv_ttk.SunValleyTtkTheme.tcl is root
-
-
-def test_with_default_root(root):
+def test_set_theme():
     sv_ttk.set_theme("dark")
-    assert sv_ttk.SunValleyTtkTheme.tcl is root
-
-
-def test_with_no_default_root(root):
-    tkinter.NoDefaultRoot()
-    with pytest.raises(RuntimeError):
-        sv_ttk.set_theme("dark")
-
-
-def test_set_theme(root):
-    sv_ttk.set_theme("dark", root=root)
     assert sv_ttk.get_theme() == "dark"
 
-    sv_ttk.set_theme("light", root=root)
+    sv_ttk.set_theme("light")
     assert sv_ttk.get_theme() == "light"
 
 
-def test_use_theme(root):
-    sv_ttk.use_dark_theme(root=root)
+def test_set_theme_using_ttk_dot_style_dot_theme_use():
+    # theme is already loaded for the default root
+    ttk.Style().theme_use("sun-valley-dark")
     assert sv_ttk.get_theme() == "dark"
 
-    sv_ttk.use_light_theme(root=root)
+    sv_ttk.set_theme("light")
+    assert ttk.Style().theme_use() == "sun-valley-light"
+
+
+def test_use_xxx_theme():
+    sv_ttk.use_dark_theme()
+    assert sv_ttk.get_theme() == "dark"
+
+    sv_ttk.use_light_theme()
     assert sv_ttk.get_theme() == "light"
 
 
-def test_toggle_theme(root):
-    sv_ttk.set_theme("dark", root=root)
+def test_toggle_theme():
+    sv_ttk.set_theme("dark")
     assert sv_ttk.get_theme() == "dark"
 
     sv_ttk.toggle_theme()
     assert sv_ttk.get_theme() == "light"
+
+
+def test_no_default_root():
+    tkinter.NoDefaultRoot()
+    with pytest.raises(RuntimeError):
+        sv_ttk.get_theme()
+
+
+def test_explicit_root():
+    root = tkinter.Tk()
+    assert sv_ttk.get_theme(root=root) == ttk.Style(root).theme_use()
+
+    sv_ttk.set_theme("dark", root=root)
+    assert sv_ttk.get_theme(root=root) == "dark"
