ec8ffae8b0b653441fad94831341881db978a3cf
jhi2 <johndrostan@outlook.com>
2024-03-28 01:17:50 +0000

    All stuff

--- tests/test_sv_ttk.py
+++ tests/test_sv_ttk.py
@@ -2,51 +2,51 @@ import tkinter
 from tkinter import ttk
 
 import pytest
-import sv_ttk
+import nativesvttk
 
 
 def test_set_theme():
-    sv_ttk.set_theme("dark")
-    assert sv_ttk.get_theme() == "dark"
+    nativesvttk.set_theme("dark")
+    assert nativesvttk.get_theme() == "dark"
 
-    sv_ttk.set_theme("light")
-    assert sv_ttk.get_theme() == "light"
+    nativesvttk.set_theme("light")
+    assert nativesvttk.get_theme() == "light"
 
 
 def test_set_theme_using_ttk_dot_style_dot_theme_use():
     # theme is already loaded for the default root
     ttk.Style().theme_use("sun-valley-dark")
-    assert sv_ttk.get_theme() == "dark"
+    assert nativesvttk.get_theme() == "dark"
 
-    sv_ttk.set_theme("light")
+    nativesvttk.set_theme("light")
     assert ttk.Style().theme_use() == "sun-valley-light"
 
 
 def test_use_xxx_theme():
-    sv_ttk.use_dark_theme()
-    assert sv_ttk.get_theme() == "dark"
+    nativesvttk.use_dark_theme()
+    assert nativesvttk.get_theme() == "dark"
 
-    sv_ttk.use_light_theme()
-    assert sv_ttk.get_theme() == "light"
+    nativesvttk.use_light_theme()
+    assert nativesvttk.get_theme() == "light"
 
 
 def test_toggle_theme():
-    sv_ttk.set_theme("dark")
-    assert sv_ttk.get_theme() == "dark"
+    nativesvttk.set_theme("dark")
+    assert nativesvttk.get_theme() == "dark"
 
-    sv_ttk.toggle_theme()
-    assert sv_ttk.get_theme() == "light"
+    nativesvttk.toggle_theme()
+    assert nativesvttk.get_theme() == "light"
 
 
 def test_no_default_root():
     tkinter.NoDefaultRoot()
     with pytest.raises(RuntimeError):
-        sv_ttk.get_theme()
+        nativesvttk.get_theme()
 
 
 def test_explicit_root():
     root = tkinter.Tk()
-    assert sv_ttk.get_theme(root=root) == ttk.Style(root).theme_use()
+    assert nativesvttk.get_theme(root=root) == ttk.Style(root).theme_use()
 
-    sv_ttk.set_theme("dark", root=root)
-    assert sv_ttk.get_theme(root=root) == "dark"
+    nativesvttk.set_theme("dark", root=root)
+    assert nativesvttk.get_theme(root=root) == "dark"
