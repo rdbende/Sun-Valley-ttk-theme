4dad3e7f27a5418a2bdf06d963644c6fbd7bd144
jhi2 <johndrostan@outlook.com>
2024-04-04 22:32:54 +0000

    Tried to make it mergeable

--- nativesvttk/__init__.py
+++ nativesvttk/__init__.py
@@ -7,6 +7,7 @@ from tkinter import ttk
 import ctypes as ct
 import darkdetect
 import platform
+import sys
 
 TCL_THEME_FILE_PATH = Path(__file__).with_name("sv.tcl").absolute()
 
@@ -61,33 +62,35 @@ def toggle_theme(root: tkinter.Tk | None = None) -> None:
 
     set_theme("light" if style.theme_use() == "sun-valley-dark" else "dark")
 def titlebar(root: tkinter.tk,theme):
-    def dark_title_bar(window):
-        window.update()
-        DWMWA_USE_IMMERSIVE_DARK_MODE = 20
-        set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
-        get_parent = ct.windll.user32.GetParent
-        hwnd = get_parent(window.winfo_id())
-        rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
-        value = 2
-        value = ct.c_int(value)
-        set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
-    def light_title_bar(window):
-        window.update()
-        DWMWA_USE_IMMERSIVE_DARK_MODE = 20
-        set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
-        get_parent = ct.windll.user32.GetParent
-        hwnd = get_parent(window.winfo_id())
-        rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
-        value = 0
-        value = ct.c_int(value)
-        set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
-    if theme not in {"dark", "light"}:
-            raise RuntimeError(f"not a valid sv_ttk theme: {theme}")
-    else:
-        if theme == "light":
-            light_title_bar(root)
-        elif theme == "dark":
-            dark_title_bar(root)
+    # Thanks to Olikonsti for theese titlebars
+    if sys.platform == "win32":
+        def dark_title_bar(window):
+            window.update()
+            DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+            set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+            get_parent = ct.windll.user32.GetParent
+            hwnd = get_parent(window.winfo_id())
+            rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
+            value = 2
+            value = ct.c_int(value)
+            set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
+        def light_title_bar(window):
+            window.update()
+            DWMWA_USE_IMMERSIVE_DARK_MODE = 20
+            set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
+            get_parent = ct.windll.user32.GetParent
+            hwnd = get_parent(window.winfo_id())
+            rendering_policy = DWMWA_USE_IMMERSIVE_DARK_MODE
+            value = 0
+            value = ct.c_int(value)
+            set_window_attribute(hwnd, rendering_policy, ct.byref(value), ct.sizeof(value))
+        if theme not in {"dark", "light"}:
+                raise RuntimeError(f"not a valid sv_ttk theme: {theme}")
+        else:
+            if theme == "light":
+                light_title_bar(root)
+            elif theme == "dark":
+                dark_title_bar(root)
 def native_theme(root:tkinter.tk):
     set_theme("light",root)
     is_dark = darkdetect.isDark()
